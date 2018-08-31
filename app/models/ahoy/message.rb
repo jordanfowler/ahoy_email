627cc5f2288e2076e1c49c945e37076874cd4250
Jordan Fowler <me@jordanfowler.co>
2018-08-31 10:59:59 -0600

    Remove ActiveRecord::Base dependency.

--- app/models/ahoy/message.rb
+++ app/models/ahoy/message.rb
@@ -1,5 +1,5 @@
 module Ahoy
-  class Message < ActiveRecord::Base
+  class Message < ApplicationRecord
     self.table_name = "ahoy_messages"
 
     belongs_to :user, AhoyEmail.belongs_to.merge(polymorphic: true)
